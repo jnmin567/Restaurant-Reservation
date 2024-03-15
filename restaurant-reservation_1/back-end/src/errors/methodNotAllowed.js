a63ed939673b1a979c9acbcdc79f7e59f26ab3b4
jnmin567 <152448979+jnmin567@users.noreply.github.com>
2024-03-14 17:57:32 -0700

    Add files via upload

---
+++ restaurant-reservation_1/back-end/src/errors/methodNotAllowed.js
@@ -0,0 +1,11 @@
+/**
+ * method "Not found" handler.
+ */
+function methodNotAllowed(req, res, next) {
+  next({
+    status: 405,
+    message: `${req.method} not allowed for ${req.originalUrl}`,
+  });
+}
+
+module.exports = methodNotAllowed;
